a3aba3fc52b3eb97f132775883b6d6f6269fe248
ivan4000 <sxln1@mail.ru>
2018-10-29 15:52:08 +0300

    JL35operators final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        int a=15;
+        int b=6;
+        System.out.println(a+b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a-b);
+        double aa=5;
+        double bb=6;
+        System.out.println(aa/bb);
+        System.out.println(a%b);
+        int c=5;
+        c=c+a;
+        System.out.println(c);
+        c+=c;
+        System.out.println(c);
+        System.out.println(5<=6);
+        System.out.println(5>6);
+        System.out.println(5==5.0);
+        Object o = new Object();
+        Object o2 = new Object();
+        System.out.println(o==o2);
+        Object o3 = o;
+        System.out.println(o==o3);
+
+
+
 
     }
 }
--- src/MainObj.java
+++ src/MainObj.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.List;
 
-public class MainObj {
+public class MainObj extends Object{
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main instanceof List);
+        System.out.println(main instanceof Object);
+
+        String s = "bla";
+        System.out.println(s + " yohohoho");
+        System.out.println("" + 5);
+        System.out.println("" + 5 + 6);
+        System.out.println(5 + 6 + "");
+        int a = 5;
+        a++;
+        System.out.println(a);
+        a--;
+        --a;
+        System.out.println(a);
+        System.out.println(a++ + ++a);
+        System.out.println(a);
+        int i = (5 > 6) ? 1 : 2;
+        System.out.println(i);
+        i = (7 > 6) ? 1 + 3 : 2;
+        System.out.println(i);
+        i = false ? 1 : 2;
+        System.out.println(i);
+
+        System.out.println(true && false);
+        System.out.println(5 < 6 && 3 < 4);
+        System.out.println(5 < 6 & 3 < 4);
+        System.out.println(5>6 | 3<4);
+        System.out.println(5>6 | 3>4);
+
+        System.out.println(3>4 || returnBool());
+        System.out.println(3>4 | returnBool());
+
+        System.out.println(!(3>4));
+        System.out.println(true ^ false);
+        System.out.println(true ^ true);
+    }
+    private static boolean returnBool(){
+        System.out.println("returnBoolean");
+        return true;
+    }
 }
